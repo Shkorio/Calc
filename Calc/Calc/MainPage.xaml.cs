091b365ae9697076e4bb05dd51dbc97253417acc
Student <Student@CAB4-3>
2022-09-12 12:36:39 +1000

    Добавьте файлы проекта.

---
+++ Calc/Calc/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void BtnOne_Clicked(object sender,EventArgs e)
+        {
+            LB1Result.Text = "1";
+
+        }
+    }
+}
